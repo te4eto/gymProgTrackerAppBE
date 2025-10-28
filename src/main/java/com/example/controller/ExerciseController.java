b71c3de51cef59cb841259b173cd9d5224c5b9d3
te4eto <astigae55@gmail.com>
2025-10-28 11:02:03 +0200

    config for prod

--- src/main/java/com/example/controller/ExerciseController.java
+++ src/main/java/com/example/controller/ExerciseController.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/exercises")
-@CrossOrigin
+@CrossOrigin(origins = "*")
 public class ExerciseController {
 
     @Autowired
--- src/main/java/com/example/controller/WorkoutSessionController.java
+++ src/main/java/com/example/controller/WorkoutSessionController.java
@@ -12,7 +12,7 @@ import java.time.LocalDate;
 import java.util.List;
 
 @RestController
-@CrossOrigin
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/sessions")
 public class WorkoutSessionController {
 
