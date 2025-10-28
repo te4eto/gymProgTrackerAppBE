85e07df62341b55d53d3b3c1292d797a58c9afa9
te4eto <astigae55@gmail.com>
2025-10-28 14:24:30 +0200

    debug setup

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,13 +1,34 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Component
+	public static class ConfigLogger implements CommandLineRunner {
+
+		@Value("${spring.datasource.url:URL_NOT_FOUND}")
+		private String dbUrl;
+
+		@Value("${spring.profiles.active:NO_PROFILE_SET}")
+		private String activeProfile;
+
+		@Override
+		public void run(String... args) throws Exception {
+			System.out.println("--- CONFIG CHECK START ---");
+			System.out.println("Active Profile: " + activeProfile);
+			System.out.println("DB URL: " + dbUrl);
+			System.out.println("--- CONFIG CHECK END ---");
+		}
 	}
 
 }
